5b2625ac35374335d86587b0d495f99387553950
jefer <jefer@192.168.0.100>
2021-11-21 21:34:27 -0400

    Login Actualizado

--- Clinica/src/Visual/LoginRegister.java
+++ Clinica/src/Visual/LoginRegister.java
@@ -11,11 +11,16 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class LoginRegister extends JFrame {
 
 	private JPanel contentPane;
 	private Dimension dimension; 
+	private JTextField txtCodigoUsuario;
+	private JTextField txtContrasena;
 
 	/**
 	 * Launch the application.
@@ -48,11 +53,44 @@ public class LoginRegister extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		ImageIcon imagen =new ImageIcon(getClass().getResource("/Imgenes/pngwing.com (1).png"));
 		Icon icono= new ImageIcon(imagen.getImage().getScaledInstance((int)1920-45,(int)1040-45,Image.SCALE_DEFAULT));
 		lblNewLabel.setIcon(icono);
 		lblNewLabel.setBounds(0, 0, 56, 16);
 		lblNewLabel.setSize((int)dimension.getWidth(),(int)dimension.getHeight()-40);
 		contentPane.add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(766, 129, 358, 276);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Codigo de Usuario");
+		lblNewLabel_1.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+		lblNewLabel_1.setBounds(92, 27, 160, 34);
+		panel.add(lblNewLabel_1);
+		
+		txtCodigoUsuario = new JTextField();
+		txtCodigoUsuario.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		txtCodigoUsuario.setBounds(29, 73, 299, 34);
+		panel.add(txtCodigoUsuario);
+		txtCodigoUsuario.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Contrase\u00F1a");
+		lblNewLabel_2.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+		lblNewLabel_2.setBounds(122, 120, 153, 26);
+		panel.add(lblNewLabel_2);
+		
+		txtContrasena = new JTextField();
+		txtContrasena.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		txtContrasena.setColumns(10);
+		txtContrasena.setBounds(29, 159, 299, 34);
+		panel.add(txtContrasena);
+		
+		JButton btnConectar = new JButton("Conectar");
+		btnConectar.setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+		btnConectar.setBounds(112, 218, 124, 25);
+		panel.add(btnConectar);
 	}
 }
