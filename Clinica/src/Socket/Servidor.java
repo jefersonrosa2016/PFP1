153d982a8b780dbc570c4e4c4f928dabee88ebe3
jefer <jefer@192.168.0.102>
2021-12-06 16:15:41 -0400

    Server mod

--- Clinica/src/Socket/Servidor.java
+++ Clinica/src/Socket/Servidor.java
@@ -2,17 +2,12 @@ package Socket;
 
 
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import Logico.Administrador;
-import Logico.Clinica;
 
 public class Servidor {
 
